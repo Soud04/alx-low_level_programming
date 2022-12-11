c804986a4b3eb96fb54d17cd733dfc2c7da13e69
Soud04 <soudjr04@gmail.com>
2022-12-11 14:03:46 -0800

    TAsk 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,9 @@
 
 
 
-/* main - prints the phrase "Programming is like building a multilingual puzzle Return: 0 if exited properly, non-zero otherwise */
+/*main - prints the phrase "Programming is like building a multilingual puzzle"*/
+
+/* Return: 0 if exited properly, non-zero otherwise*/
 
 
 
@@ -17,4 +19,3 @@ int main(void)
 
 
 }
-
