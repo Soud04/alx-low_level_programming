f4bbc1057d1355ec84ac9998dcd71554c3f0057c
Soud04 <soudjr04@gmail.com>
2022-12-11 22:23:48 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,19 +2,25 @@
 
 
 
-/*main - prints the phrase "Programming is like building a multilingual puzzle"*/
+/**
 
-/* Return: 0 if exited properly, non-zero otherwise*/
+ *main - is the entry point
 
+ *Return: Always 0 (Success)
 
+ */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+
+
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 
 
 
