c257dbe1ca716984630e64c24096a3a8d02df693
Soud04 <soudjr04@gmail.com>
2022-12-12 12:11:43 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  * C program that prints exactly with proper grammar, but the outcome is a piece of art
-
  *
-
  * Return: Always zero
-
  */
 
-
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
