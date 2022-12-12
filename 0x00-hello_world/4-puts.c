8b0ff44a83cf0de9bd785b25b17712ba14ac59be
Soud04 <soudjr04@gmail.com>
2022-12-12 11:37:19 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,15 @@
-#include <stdio.h>
-
-
-
+#include <studio.h>
 /**
-
- * main - Entry point
-
+ * main - Entry piont
  *
+ *The C program that prints exactly "Programming is like building a multilingual puzzle
+ *
+ *return (0) - return zero at the end of the program
 
- * Return: Always 0 (Success)
-
- */
+*/
 
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
