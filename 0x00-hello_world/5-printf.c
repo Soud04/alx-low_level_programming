f906c7cd553c49b4cf93400e7b0028a474a19481
Soud04 <soudjr04@gmail.com>
2022-12-12 12:40:03 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 /**
  * main - Entry point
  *
- * Prints with proper grammar, but the outcome is a piece of art
+ * Prints with proper grammar, but the outcome is a piece of art,
  *
  * Return: Always zero
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
