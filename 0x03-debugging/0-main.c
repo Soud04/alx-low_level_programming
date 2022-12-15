4995b8051fa47a5eafbb756604049b4e9ab53165
Soud04 <soudjr04@gmail.com>
2022-12-15 05:43:21 -0800

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,27 +5,6 @@
 * Return: 0
 */
 
-void positive_or_negative(int i)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
-}
-
 int main(void)
 {
 	int i;
